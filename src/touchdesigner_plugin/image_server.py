aa570f01a51fe07e137a0c49a438d99cb9c98ce2
Niclas Wesemann <niclas.wesemann@motius.de>
2024-01-12 11:44:49 +0100

    write image server class
    Signed-off-by: Niclas Wesemann <niclas.wesemann@motius.de>

--- src/touchdesigner_plugin/image_server.py
+++ src/touchdesigner_plugin/image_server.py
@@ -5,46 +5,60 @@ import struct
 from touchdesigner_plugin.constants import HOST, PORT, JPEG_ENCODE_QUALITY_PERCENT
 
 
-def send_image(client_socket, image_path):
-    # ToDo generate image here
-    image = cv.imread(image_path)
+class ImageServer:
+    def __init__(self, use_webcam: int = 0):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.bind((HOST, PORT))
+        self.server_socket.listen(3)
 
-    # NOTE: decrease JPEG_ENCODE_QUALITY_PERCENT to increase performance
-    _, image_data = cv.imencode(
-        '.jpg', image, [int(cv.IMWRITE_JPEG_QUALITY), JPEG_ENCODE_QUALITY_PERCENT]
-    )
+        self.use_webcam = use_webcam
+        if self.use_webcam:
+            self.cap = cv.VideoCapture(use_webcam)
+        else:
+            self.image_path = "imgs/IMG_7418_1280x720.jpg"
 
-    image_size = struct.pack("!I", len(image_data))
-    client_socket.send(image_size)
-    client_socket.send(image_data.tobytes())
+        print(f"Server listening on {HOST}:{PORT}")
 
+    def send_image(self, client_socket):
+        # ToDo generate image here
+        if not self.use_webcam:
+            image = cv.imread(self.image_path)
+        else:
+            ret, image = capture.read()
 
-def main():
-    # Create socket
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((HOST, PORT))
-    server_socket.listen(3)
+        # NOTE: decrease JPEG_ENCODE_QUALITY_PERCENT to increase performance
+        _, image_data = cv.imencode(
+            '.jpg', image, [int(cv.IMWRITE_JPEG_QUALITY), JPEG_ENCODE_QUALITY_PERCENT]
+        )
 
-    print(f"Server listening on {HOST}:{PORT}")
+        image_size = struct.pack("!I", len(image_data))
+        client_socket.send(image_size)
+        client_socket.send(image_data.tobytes())
 
-    while True:
-        client_socket, client_address = server_socket.accept()
-        print(f"Connection from {client_address}")
+    def run(self):
+        while True:
+            client_socket, client_address = self.server_socket.accept()
+            print(f"Connection from {client_address}")
 
-        try:
-            # Receive prompt from client
-            prompt = client_socket.recv(2048).decode()
-            print(f"Received prompt: {prompt}")
+            try:
+                # Receive prompt from client
+                prompt = client_socket.recv(2048).decode()
+                print(f"Received prompt: {prompt}")
 
-            # Send image to client
-            send_image(
-                client_socket, "imgs/IMG_7418_1280x720.jpg"
-            )
+                # Send image to client
+                self.send_image(client_socket)
 
-            print("Image sent successfully")
-        finally:
-            client_socket.close()
+                print("Image sent successfully")
+            except Exception as e:
+                print(f"Exception: {e}")
+            finally:
+                client_socket.close()
+
+    def __del__(self):
+        print("Closing server socket..")
+        self.server_socket.close()
 
 
 if __name__ == "__main__":
-    main()
+    image_server = ImageServer()
+    image_server.run()
