5224496a03ce988f22ef491470c6781b4518a739
Niclas Wesemann <niclas.wesemann@motius.de>
2024-01-12 11:50:58 +0100

    make prompt client class
    Signed-off-by: Niclas Wesemann <niclas.wesemann@motius.de>

--- src/touchdesigner_plugin/prompt_client.py
+++ src/touchdesigner_plugin/prompt_client.py
@@ -6,48 +6,47 @@ import struct
 from touchdesigner_plugin.constants import PORT, HOST
 
 
-def receive_image(server_socket):
-    # Receive the 4-byte image size
-    image_size_data = server_socket.recv(4)
+class PromptClient:
+    def __init__(self):
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect((HOST, PORT))
 
-    # Unpack the image size as a 4-byte integer
-    image_size = struct.unpack("!I", image_size_data)[0]
+    def run(self, prompt: str):
+        try:
+            # Send prompt to server
+            self.client_socket.send(prompt.encode())
 
-    # Receive image data
-    image_data = b""
-    while len(image_data) < image_size:
-        chunk = server_socket.recv(min(image_size - len(image_data), 4096))
-        if not chunk:
-            break
-        image_data += chunk
+            # Receive image from server
+            self.receive_image()
 
-    # Decode the image data using cv2.imdecode
-    image = cv2.imdecode(np.frombuffer(image_data, dtype=np.uint8), cv2.IMREAD_COLOR)
+            print("Image received successfully")
+        except Exception as e:
+            print(f"Exception: {e}")
 
-    # Save the received image
-    cv2.imwrite("received_image.jpg", image)
+    def receive_image(self):
+        image_size_data = self.client_socket.recv(4)
 
+        # Unpack the image size as a 4-byte integer
+        image_size = struct.unpack("!I", image_size_data)[0]
 
-def main():
+        # Receive image data
+        image_data = b""
+        while len(image_data) < image_size:
+            chunk = self.client_socket.recv(min(image_size - len(image_data), 4096))
+            if not chunk:
+                break
+            image_data += chunk
 
-    # Prompt to send to the server
-    prompt = "Send me an image!"
+        # Decode the image data using cv2.imdecode
+        image = cv2.imdecode(np.frombuffer(image_data, dtype=np.uint8), cv2.IMREAD_COLOR)
 
-    # Create socket
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((HOST, PORT))
+        # Save the received image
+        cv2.imwrite("received_image.jpg", image)
 
-    try:
-        # Send prompt to server
-        client_socket.send(prompt.encode())
-
-        # Receive image from server
-        receive_image(client_socket)
-
-        print("Image received successfully")
-    finally:
-        client_socket.close()
+    def __del__(self) -> None:
+        self.client_socket.close()
 
 
 if __name__ == "__main__":
-    main()
+    prompt_client = PromptClient()
+    prompt_client.run("Test prompt!!")
